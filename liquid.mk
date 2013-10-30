0fb88debfa461fc0770e3b525e7756f2aadea67e
Sean Hoyt <seanhoyt963@gmail.com>
2013-10-30 13:31:37 -0500

    Liquify

---
+++ liquid.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The LiquidSmooth Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_tablet.mk)
+
+# Inherit GSM stuff
+$(call inherit-product, vendor/liquid/config/common_gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/deb/full_deb.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := deb
+PRODUCT_NAME := liquid_deb
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT=google/razorg/deb:4.3.1/JLS36I/841738:user/release-keys PRIVATE_BUILD_DESC="razorg-user 4.3.1 JLS36I 841738 release-keys"
+
