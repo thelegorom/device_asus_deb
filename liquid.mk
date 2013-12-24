ba4b0de3c1098fcde9573244bbfa0e671c909fb7
Sean Hoyt <seanhoyt963@gmail.com>
2013-12-24 14:33:50 -0600

    Readd noauto_da_alloc
    to fix horrific I/O performance

--- liquid.mk
+++ liquid.mk
@@ -37,7 +37,7 @@ PRODUCT_BRAND := google
 PRODUCT_NAME := liquid_deb
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
-PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.vs980.$(shell date +%m%d%y).$(shell date +%H%M%S)
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=liquid.deb.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 # override
 PRODUCT_BUILD_PROP_OVERRIDES += \
